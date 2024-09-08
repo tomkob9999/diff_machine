61baa0c186a2d52ba1a84512083b1b5bc269d69f
tokob9999 <96751911+tomkob9999@users.noreply.github.com>
2024-09-08 15:22:31 +0900

    1.0.6
    - avoid recalculations of constant term, which is most expensive

--- diff_machine.py
+++ diff_machine.py
@@ -3,7 +3,7 @@
 #
 # Description: Calculates difference equation based on the order, target row and initial values specified
 #
-# Version: 1.0.5
+# Version: 1.0.6
 # Author: Tomio Kobayashi
 # Last Update: 2024/9/8
 
@@ -14,23 +14,44 @@ class diff_machine:
     def __init__(self):
         self.memo = {}
         self.memo_found = 0
+#         self.num_get_diff = 0
+        self.fix_term = 0
+        self.order = 0
         
     def clean_memo(self):
         self.memo = {}
 
     def calc(x1, x2, exp=False):
         return x1-x2 if not exp else x1/x2
-    
+
+# deprecating  this does not use fixed_term, but the performance is almost the same
+#     def get_diff(self, ar, i, order, order_exp=[], enable_memo=True):
+# #         self.num_get_diff += 1
+#         if enable_memo and (i, order) in self.memo:
+#             self.memo_found += 1
+#             return self.memo[(i, order)]
+#         elif order == 1:
+#             return diff_machine.calc(ar[i-1], ar[i-2], order in order_exp)
+#         else:
+#             ret = diff_machine.calc(self.get_diff(ar, i, order-1, order_exp, enable_memo), self.get_diff(ar, i-1, order-1, order_exp, enable_memo), order in order_exp)
+#             self.memo[(i, order)] = ret
+#             return ret
+        
     def get_diff(self, ar, i, order, order_exp=[], enable_memo=True):
+#         self.num_get_diff += 1
         if enable_memo and (i, order) in self.memo:
             self.memo_found += 1
             return self.memo[(i, order)]
-        
-        if order == 1:
+        elif order == self.order and self.fix_term != 0:
+            return self.fix_term
+        elif order == 1:
             return diff_machine.calc(ar[i-1], ar[i-2], order in order_exp)
         else:
             ret = diff_machine.calc(self.get_diff(ar, i, order-1, order_exp, enable_memo), self.get_diff(ar, i-1, order-1, order_exp, enable_memo), order in order_exp)
             self.memo[(i, order)] = ret
+            if self.order == order and self.fix_term == 0:
+                self.fix_term = ret
+                print("fix_term", self.fix_term)
             return ret
 
     def diff_coef(n, order):
@@ -50,6 +71,7 @@ class diff_machine:
         order = len(init)-1
         ar = np.zeros(target+1)
         dd = diff_machine()
+        dd.order = order
         for k, v in init.items():
             ar[k] = v
         for i in range(order+1, target+1, 1):
@@ -66,12 +88,47 @@ class diff_machine:
                 ar[i] = ar[i-1] * order_cum
             else:
                 ar[i] = ar[i-1] + order_cum
+#         print("dd.fix_term", dd.fix_term)
+#         print("len(ar)", len(ar))
         return ar
+    
+#     # Returns value
+#     def solve(target, init, order_exp=[]):
+#         return diff_machine.solve_array(target, init, order_exp)[-1]
     
     # Returns value
     def solve(target, init, order_exp=[]):
-        return diff_machine.solve_array(target, init, order_exp)[-1]
-    
+        order = len(init)-1
+        ar = np.zeros(order+2)
+        dd = diff_machine()
+        dd.order = order
+        for k, v in init.items():
+            ar[k] = v
+        for i in range(order+1, target+1, 1):
+            order_cum = 0
+            for ii in range(order, 0, -1):
+#                 order_cum += dd.get_diff(ar, order+1, ii)
+                if ii+1 in order_exp:
+                    if order_cum == 0:
+                        order_cum = 1
+                    order_cum *= dd.get_diff(ar, order+1, ii, order_exp)
+                else:
+                    order_cum += dd.get_diff(ar, order+1, ii, order_exp)
+
+            dd.clean_memo()
+#             ar[order+1] = ar[order] + order_cum
+            if 1 in order_exp:
+#                 ar[i] = ar[i-1] * order_cum
+                ar[order+1] = ar[order] * order_cum
+            else:
+#                 ar[i] = ar[i-1] + order_cum
+                ar[order+1] = ar[order] + order_cum
+                
+            for ii in range(order+1):
+                ar[ii] = ar[ii+1]
+#         print("len(ar)", len(ar))
+        return ar[order+1]
+
 # Notations throughout
 #
 # Difference variables are expressed like differential equation notations
@@ -84,46 +141,51 @@ class diff_machine:
 #
 # Closed form: y=3*x^2+7*x+11 (1 step=1)
 # Difference equation: y'=y''+y''', y(0)=11, y(1)=21, y(2)=34
-res = diff_machine.solve(4, {0:11, 1:21, 2:34})
-print("res", res)
-# Same as above except step=0.01
-res = diff_machine.solve(21, {0:0.011, 1:0.021, 2:0.034})
-print("res", res)
-#
-# Closed form: y=x^2 (1 step=1)
-# Difference equation: y'=y''+y''', y(0)=1, y(1)=1, y(2)=4
-# res = diff_machine.solve_pure(9, {0:0, 1:1, 2:4})
-ar = diff_machine.solve_array(4, {0:0, 1:1, 2:4})
-print("ar", ar)
-#
-# Closed form: y=4x^3+3x^2
-res = diff_machine.solve_array(4, {0:0, 1:7, 2:44, 3:135})
-# res = diff_machine.solve_array(5, {0:0, 1:0.07, 2:0.44, 3:1.35})
-# res = diff_machine.solve_array(5, {0:0, 1:0.034, 2:0.152, 3:0.378})
-print("res", res)
+# res = diff_machine.solve(4, {0:11, 1:21, 2:34})
+# print("res", res)
+# res = diff_machine.solve_array(4, {0:11, 1:21, 2:34})
+# print("res", res)
+# # Same as above except step=0.01
+# res = diff_machine.solve(10000, {0:0.011, 1:0.021, 2:0.034})
+# print("res", res)
+# #
+# # Closed form: y=x^2 (1 step=1)
+# # Difference equation: y'=y''+y''', y(0)=1, y(1)=1, y(2)=4
+# ar = diff_machine.solve_array(20, {0:0, 1:1, 2:4})
+# print("ar", ar)
+# #
+# # Closed form: y=4x^3+3x^2
+# res = diff_machine.solve_array(4, {0:0, 1:7, 2:44, 3:135})
+# # res = diff_machine.solve_array(5, {0:0, 1:0.07, 2:0.44, 3:1.35})
+# # res = diff_machine.solve_array(5, {0:0, 1:0.034, 2:0.152, 3:0.378})
+# print("res", res)
 
-# Closed form: y=5x^5+4x^4+3x^3+2x^2+1
-# res = diff_machine.solve_array(6, {0:0, 1:15, 2:258, 3:1641, 4:6372, 5:18555})
-res = diff_machine.solve_array(6, {0:0, 1:0.12345, 2:0.312, 3:0.60555, 4:1.0656, 5:1.78125})
-print("res", res)
+# # Closed form: y=5x^5+4x^4+3x^3+2x^2+1
+import time
+start_time = time.time()
+res = diff_machine.solve_array(10000, {0:0, 1:0.12345, 2:0.312, 3:0.60555, 4:1.0656, 5:1.78125})
+# res = diff_machine.solve(10000, {0:0, 1:0.12345, 2:0.312, 3:0.60555, 4:1.0656, 5:1.78125})
+# print("res", res)
+air_time = time.time() - start_time
+print(f"Execution Time: {air_time:.6f} seconds")
 
-# Closed form: y=2^x (1 step=1)
-# Difference equation: y(x)=y(x-1)**2/y(x-2), y(0)=1, y(1)=2
-ar = diff_machine.solve(40, {0:1, 1:2}, order_exp=[1])
-print("ar", ar)
-ar = diff_machine.solve_array(100, {0:1, 1:1.01}, order_exp=[1])
-print("ar", ar)
-ar = diff_machine.solve(100, {0:1, 1:1.01}, order_exp=[1])
-print("ar", ar)
-#
-# Closed form: 4*2^(x*3) (1 step=1)
-ar = diff_machine.solve(10, {0:4, 1:32}, order_exp=[1])
-print("ar", ar)
-ar = diff_machine.solve_array(10, {0:4, 1:32}, order_exp=[1])
-print("ar", ar)
-# 
-# Closed form: y(x)=2^(x-1)+(x-1)
-ar = diff_machine.solve_array(10, {0:1, 1:3, 2:6, 3:11}, order_exp=[3])
-print("ar", ar)
-ar = diff_machine.solve(10, {0:1, 1:3, 2:6, 3:11}, order_exp=[3])
-print("ar", ar)
+# # Closed form: y=2^x (1 step=1)
+# # Difference equation: y(x)=y(x-1)**2/y(x-2), y(0)=1, y(1)=2
+# ar = diff_machine.solve(40, {0:1, 1:2}, order_exp=[1])
+# print("ar", ar)
+# ar = diff_machine.solve_array(100, {0:1, 1:1.01}, order_exp=[1])
+# print("ar", ar)
+# ar = diff_machine.solve(100, {0:1, 1:1.01}, order_exp=[1])
+# print("ar", ar)
+# #
+# # Closed form: 4*2^(x*3) (1 step=1)
+# ar = diff_machine.solve(10, {0:4, 1:32}, order_exp=[1])
+# print("ar", ar)
+# ar = diff_machine.solve_array(10, {0:4, 1:32}, order_exp=[1])
+# print("ar", ar)
+# # 
+# # Closed form: y(x)=2^(x-1)+(x-1)
+# ar = diff_machine.solve_array(10, {0:1, 1:3, 2:6, 3:11}, order_exp=[3])
+# print("ar", ar)
+# ar = diff_machine.solve(10, {0:1, 1:3, 2:6, 3:11}, order_exp=[3])
+# print("ar", ar)
